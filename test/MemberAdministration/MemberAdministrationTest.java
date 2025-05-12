e945ae6a6295c4168d408dd717041f90b3e33b0c
MaVirgil <magnusvirgil@hotmail.com>
2025-05-12 13:56:49 +0200

    Wrote automatic tests for Member and MembershipFee classes

--- test/MemberAdministration/MemberAdministrationTest.java
+++ test/MemberAdministration/MemberAdministrationTest.java
@@ -1,47 +1,128 @@
 package MemberAdministration;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+import java.lang.annotation.Annotation;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class MemberAdministrationTest {
 
 
     //member tests
-    @Test
-    void expectedExpirationDate() {
 
-        LocalDate expectedExpirationDate = LocalDate.now().plusYears(1);
+    @Test
+    @Order(1)
+    void loadMembers() {
+
+        final int EXPECTED_AMOUNT_OF_MEMBERS = 3;
+
+        String path = "./resources/MemberList.csv";
+        MemberList memberList = new MemberList(path);
+
+        assertEquals(EXPECTED_AMOUNT_OF_MEMBERS, memberList.getMemberList().size());
+    }
+
+    @Test
+    @Order(2)
+    void idGeneration() {
+
+        final int EXPECTED_ID = 4;
+        String path = "./resources/MemberList.csv";
+        MemberList memberList = new MemberList(path);
+
         LocalDate birthday = LocalDate.of(1969,8,14);
         Member member = new Member("Jørgen Mikkelsen", birthday, true);
 
-        assertEquals(expectedExpirationDate, member.getMembershipExpirationDate());
+        assertEquals(EXPECTED_ID, member.getMemberId());
     }
 
     @Test
-    void IdGeneration() {
-        //first member that has an ID of 11
-        Member member1 = new Member(11,"Erik Jensen",
-                LocalDate.of(1975,3,13),
-                LocalDate.now(),
-                LocalDate.now().plusYears(1),
-                true,
-                true);
+    @Order(3)
+    void idGenerationWithExistingMemberList() {
 
-        //second member that should get an id of 12
+        final int EXPECTED_ID = 5;
+
         LocalDate birthday = LocalDate.of(1969,8,14);
-        Member member2 = new Member("Jørgen Mikkelsen", birthday, true);
+        Member member = new Member("Jørgen Mikkelsen", birthday, true);
 
-        assertEquals(12, member2.getMemberId());
+        assertEquals(EXPECTED_ID, member.getMemberId());
     }
+
+    @Test
+    void expectedExpirationDate() {
+        
+        final LocalDate EXPECTED_EXPIRATION_DATE = LocalDate.now().plusYears(1);
+        LocalDate birthday = LocalDate.of(1969,8,14);
+        Member member = new Member("Jørgen Mikkelsen", birthday, true);
+
+        assertEquals(EXPECTED_EXPIRATION_DATE, member.getMembershipExpirationDate());
+    }
+
 
     //MembershipFee test
 
     @Test
     void memberShipFeeJunior() {
 
+        final double EXPECTED_PRICE = 1000.00;
+
+        //test for 17-year-old (should get junior price)
+        LocalDate birthdaySeventeenYearsOld = LocalDate.now().minusYears(17);
+        Member member = new Member("Lilleper", birthdaySeventeenYearsOld, false);
+        assertEquals(EXPECTED_PRICE,MembershipFee.calculatePayment(member));
+
+        //test for 18-year-old (should NOT get junior price)
+        LocalDate birthdayEighteenYearsOld = LocalDate.now().minusYears(18);
+        Member member2 = new Member("Storeper", birthdayEighteenYearsOld, false);
+        assertNotEquals(EXPECTED_PRICE, MembershipFee.calculatePayment(member2));
+    }
+
+    @Test
+    void memberShipFeeSenior() {
+
+        final double EXPECTED_PRICE = 1600.00;
+
+        //test for 18-year-old (should get senior price)
+        LocalDate birthdayEighteenYearsOld = LocalDate.now().minusYears(18);
+        Member member = new Member("Storeper", birthdayEighteenYearsOld, false);
+        assertEquals(EXPECTED_PRICE,MembershipFee.calculatePayment(member));
+
+        //test for 59-year-old (should get senior price)
+        LocalDate birthdayFiftyNineYearsOld = LocalDate.now().minusYears(59);
+        Member member2 = new Member("Kæmpeper", birthdayFiftyNineYearsOld, false);
+        assertEquals(EXPECTED_PRICE,MembershipFee.calculatePayment(member2));
+
+        //test for 17-year-old (should NOT get senior price)
+        LocalDate birthdaySeventeenYearsOld = LocalDate.now().minusYears(17);
+        Member member3 = new Member("Lilleper", birthdaySeventeenYearsOld, false);
+        assertNotEquals(EXPECTED_PRICE, MembershipFee.calculatePayment(member3));
+
+        //test for 60-year-old (should NOT get senior price)
+        LocalDate birthdaySixtyYearsOld = LocalDate.now().minusYears(60);
+        Member member4 = new Member("Gamleper", birthdaySixtyYearsOld, false);
+        assertNotEquals(EXPECTED_PRICE, MembershipFee.calculatePayment(member4));
+    }
+
+    @Test
+    void memberShipFeeDiscount() {
+
+        final double EXPECTED_PRICE = 1600.00 * (1-0.25);
+
+        //test for 60-year-old (should get discount)
+        LocalDate birthdaySixtyYearsOld = LocalDate.now().minusYears(60);
+        Member member = new Member("Gamleper", birthdaySixtyYearsOld, false);
+        assertEquals(EXPECTED_PRICE, MembershipFee.calculatePayment(member));
+
+        //test for 59-year-old (should NOT get discount)
+        LocalDate birthdayFiftyNineYearsOld = LocalDate.now().minusYears(59);
+        Member member2 = new Member("Kæmpeper", birthdayFiftyNineYearsOld, false);
+        assertNotEquals(EXPECTED_PRICE,MembershipFee.calculatePayment(member2));
     }
 
 }
