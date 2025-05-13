975fb04a33e252a886ca4495b0c04a9443f56e69
Mbroholm <mibr0005@stud.kea.dk>
2025-05-13 09:55:11 +0200

    moved TestMemberList.csv to testFile directory;

--- test/MemberAdministration/MemberAdministrationTest.java
+++ test/MemberAdministration/MemberAdministrationTest.java
@@ -22,7 +22,7 @@ class MemberAdministrationTest {
     @Order(1)
     void loadMembers() {
 
-        String path = "./resources/TestMemberList.csv";
+        String path = "./resources/testFiles/TestMemberList.csv";
         MemberList memberList = new MemberList(path);
 
         assertEquals(EXPECTED_AMOUNT_OF_MEMBERS_IN_FILE, memberList.getMemberList().size());
